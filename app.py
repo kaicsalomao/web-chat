c7a202d34c3efea672863e540621c90138da5389
Kaic Salomao <ikaicsh@gmail.com>
2024-02-02 16:58:30 -0300

    enhance code and clear deprecated codes

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-import json
-
-# Importa as bibliotecas necessárias
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
-# Cria a aplicação Flask
+
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "secret!"
-# Cria o objeto SocketIO
+
 io = SocketIO(app)
 messages = []
 
@@ -18,22 +15,20 @@ def home():
     return render_template("index.html")
 
 
-# Define o evento de conexão
+# Evento de entrada -> connect
 @io.on("connect")
 def connect():
-    # Envia as mensagens para o cliente
+    # Envia o array de mensagens para o cliente (simulando um banco de dados)
     io.emit("return_messages", messages)
 
 
-# Define o evento de enviar mensagem
+# Evento de enviar as mensagens para todos os clientes 
 @io.on("sendMessage")
 def send_message_handle(msg):
     messages.append(msg)
     # Envia a mensagem para todos os clientes conectados
     io.emit("getMessage", msg)
 
-
-# Executa a aplicação
+ 
 if __name__ == "__main__":
-    # Inicia o servidor em modo de depuração
     io.run(app, debug=True)
